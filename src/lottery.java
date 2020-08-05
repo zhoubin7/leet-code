cf91fec050bd598bbf81c367c9ab704f37fc631c
pug7 <249307322@qq.com>
2020-08-06 00:10:49 +0800

    方法优化：处理0的情况

--- src/lottery.java
+++ src/lottery.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @Package: PACKAGE_NAME
@@ -13,25 +10,36 @@ import java.util.Random;
  */
 public class lottery {
     public static void main(String[] args) {
-        int r1 = new Random().nextInt(35);
-        int r2 = new Random().nextInt(35);
-        int r3 = new Random().nextInt(35);
-        int r4 = new Random().nextInt(35);
-        int r5 = new Random().nextInt(35);
-        List<Integer> redToday = new ArrayList<>();
-        redToday.add(r1);
-        redToday.add(r2);
-        redToday.add(r3);
-        redToday.add(r4);
-        redToday.add(r5);
-        Collections.sort(redToday);
+        Set<Integer> reds = new TreeSet<>();
+        Set<Integer> bules = new TreeSet<>();
+        for (; ; ) {
+            if (reds.size() ==5){
+                break;
+            }
+            int red = randomRed();
+            if (red == 0) {
+                continue;
+            }
+            reds.add(red);
+        }
+        for (; ; ) {
+            if (bules.size() ==2){
+                break;
+            }
+            int blue = randomBlue();
+            if (blue == 0) {
+                continue;
+            }
+            bules.add(blue);
+        }
+        System.out.println(reds+" | "+bules);
+    }
 
-        int b1 = new Random().nextInt(12);
-        int b2 = new Random().nextInt(12);
-        List<Integer> blueToday = new ArrayList<>();
-        blueToday.add(b1);
-        blueToday.add(b2);
-        Collections.sort(blueToday);
-        System.out.println(redToday+" | "+blueToday);
+    private static int randomRed() {
+        return new Random().nextInt(35);
+    }
+
+    private static int randomBlue() {
+        return new Random().nextInt(12);
     }
 }
