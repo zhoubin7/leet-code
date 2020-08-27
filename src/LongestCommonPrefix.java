d4e891080d05772e3a19d0ce082083fc0cdcad2d
zhoubin <zhoubin@qianlima.com>
2020-08-27 10:40:22 +0800

    最长公共前缀

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,48 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: LongestCommonPrefix
+ * @Description: 最长公共前缀
+ * @Author: pug
+ * @Date: 2020/8/25 16:13
+ * @Version: 1.0
+ */
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[]strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());
+        }
+        int low = 0, high = minLength;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (isCommonPrefix(strs, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return strs[0].substring(0, low);
+    }
+
+    public static boolean isCommonPrefix(String[] strs, int length) {
+        String str0 = strs[0].substring(0, length);
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            String str = strs[i];
+            for (int j = 0; j < length; j++) {
+                if (str0.charAt(j) != str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
