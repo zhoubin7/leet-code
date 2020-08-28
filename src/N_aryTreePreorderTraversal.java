07187584095777fd1a77644503b3ae92a743bc82
zhoubin <zhoubin@qinalima.com>
2020-08-28 10:59:22 +0800

    N叉树的前序遍历

---
+++ src/N_aryTreePreorderTraversal.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: N叉树的前序遍历
+ * @Description: TODO(用一句话描述该文件做什么)
+ * @Author: pug
+ * @Date: 2020/8/18 15:15
+ * @Version: 1.0
+ */
+public class N_aryTreePreorderTraversal {
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        if(root != null){
+            res.add(root.val);
+            int size = root.children.size();
+            for(int i=0; i<size; i++){
+                preorder(root.children.get(i));
+            }
+        }
+        return res;
+    }
+
+}
