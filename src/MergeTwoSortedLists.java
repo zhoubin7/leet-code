266b9cfccd8a4db447def8018c2fde6931652f4d
zhoubin <zhoubin@qinalima.com>
2020-08-28 14:08:02 +0800

    合并两个有序链表： 递归，直到两个链表都为空 遍历，用一个临时节点来牵着两个链表合并

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,72 @@
+import org.jetbrains.annotations.Contract;
+
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: MergeTwoSortedLists
+ * @Description: 合并两个有序链表
+ * @Author: pug
+ * @Date: 2020/8/28 11:32
+ * @Version: 1.0
+ */
+public class MergeTwoSortedLists {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1,new ListNode(2,new ListNode(4,null)));
+        ListNode l2 = new ListNode(1,new ListNode(3,new ListNode(4,null)));
+        ListNode listNode = mergeTwoLists(l1, l2);
+        System.out.println(listNode);
+        ListNode listNode1 = mergeTwoLists1(l1, l2);
+        System.out.println(listNode1);
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }else if (l2 == null) {
+            return l1;
+        }else if (l1.val<l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+}
