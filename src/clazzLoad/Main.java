966a0b42d03d66ee722fd9b09fcce9cfd247b6ec
zhoubin <zhoubin@qinalima.com>
2020-09-07 16:01:50 +0800

    cc

---
+++ src/clazzLoad/Main.java
@@ -0,0 +1,11 @@
+package clazzLoad;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        System.in.read();
+    }
+}
