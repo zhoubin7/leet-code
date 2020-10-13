0fd4e7b5e0c6bc5900e1f5453a1573d94ceb51f2
zhoubin <zhoubin@qinalima.com>
2020-10-13 17:20:55 +0800

    24. 两两交换链表中的节点

---
+++ src/Algorithm/LinkedList/mid/SwapNodesinPairs.java
@@ -0,0 +1,66 @@
+package Algorithm.LinkedList.mid;
+
+/**
+ * @Package: Algorithm.LinkedList.mid
+ * @ClassName: SwapNodesinPairs
+ * @Description: 24. 两两交换链表中的节点
+ * @Author: pug
+ * @Date: 2020/10/13 16:30
+ * @Version: 1.0
+ */
+public class SwapNodesinPairs {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = null;
+        ListNode listNode = swapPairs1(l1);
+        System.out.println(listNode);
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    public static ListNode swapPairs1(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
