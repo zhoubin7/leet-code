cb0d974bac1ae4b6e1c0fe6c69a72a973541c50f
zhoubin <zhoubin@qinalima.com>
2020-10-23 09:53:35 +0800

    376. 摆动序列

--- src/MyTest.java
+++ src/MyTest.java
@@ -12,21 +12,22 @@ import java.time.LocalDateTime;
 public class MyTest {
 
     public static void main(String[] args) {
-        System.out.println("-----------开始推送--------------");
-        long stime = System.currentTimeMillis();
-
-
-        for (int i = 0; i < 100; i++) {
-            //通过ThreadPoolTaskExecutor.excute执行一个业务方法
-            //看日志的情况这100个线程其实执行的很慢：可能10秒钟
-        }
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Long endTime = System.currentTimeMillis();
-        System.out.println("订阅推送任务耗时："+ (endTime - stime) +"ms");
+        System.out.println();
+//        System.out.println("-----------开始推送--------------");
+//        long stime = System.currentTimeMillis();
+//
+//
+//        for (int i = 0; i < 100; i++) {
+//            //通过ThreadPoolTaskExecutor.excute执行一个业务方法
+//            //看日志的情况这100个线程其实执行的很慢：可能10秒钟
+//        }
+//        try {
+//            Thread.currentThread().join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        Long endTime = System.currentTimeMillis();
+//        System.out.println("订阅推送任务耗时："+ (endTime - stime) +"ms");
         //这个耗时打印出来可能就5ms
 //        a:
 //        for (int i = 0; i < 5; i++) {
