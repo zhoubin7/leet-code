3614f735b808c15a9a234a9d88d44fb7c173ddd3
zhoubin <zhoubin@qinalima.com>
2020-10-29 16:39:23 +0800

    242. 有效的字母异位词

---
+++ src/Algorithm/sort/simple/ValidAnagram.java
@@ -0,0 +1,64 @@
+package Algorithm.sort.simple;
+
+import java.util.Arrays;
+
+/**
+ * @Package: Algorithm.sort.simple
+ * @ClassName: ValidAnagram
+ * @Description: 242. 有效的字母异位词
+ * @Author: pug
+ * @Date: 2020/10/29 16:19
+ * @Version: 1.0
+ */
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram", t = "nagaram";
+        boolean anagram = isAnagram(s, t);
+        System.out.println(anagram);
+    }
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+
+    public static boolean isAnagram1(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
