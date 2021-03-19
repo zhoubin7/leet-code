a3dbd6c15b21f94e017fc712241dc36d12afabc1
zhoubin <zhoubin@qianlima.com>
2021-03-19 10:55:35 +0800

    最小k个数

--- src/SmallestKLCCI.java
+++ src/SmallestKLCCI.java
@@ -14,21 +14,21 @@ public class SmallestKLCCI {
 
     public static void main(String[] args) {
         int[] arr = new int[100000000];
-//        Random r = new Random(10000000);
         for (int i = 0; i < 100000000; i++) {
             arr[i] = i;
         }
         long st = System.currentTimeMillis();
-        Arrays.sort(arr);
-        Arrays.stream(arr).limit(10).forEach(System.out::println);
-//        ks(arr, 10);
+//        Arrays.sort(arr);
+//        Arrays.stream(arr).limit(10).forEach(System.out::println);
+        ks(arr, 10);
         System.out.println(System.currentTimeMillis() - st+"ms");
     }
 
     public static void ks(int[] arr, int k){
-
+        //定义输出的结果数组
         int[]res = new int[k];
 
+        //定义优先队列，排序方式为升序
         PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
@@ -36,11 +36,13 @@ public class SmallestKLCCI {
             }
         });
 
+        //先拿出k个数放入队列中
         for (int i = 0; i < k; ++i) {
             queue.offer(arr[i]);
         }
 
 
+        //循环比较数组中每一个元素与队顶的元素大小，peek取出队顶元素，队顶大，删之。并放入数组中的元素
         for (int i = k; i < arr.length; ++i) {
             if (queue.peek() > arr[i]){
                 queue.poll();
@@ -48,7 +50,7 @@ public class SmallestKLCCI {
             }
         }
 
-
+        //取出队列中k个数字
         for (int i = 0; i < k; ++i) {
             res[i] = queue.poll();
         }
