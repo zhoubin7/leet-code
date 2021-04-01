0c0f4824bff03e6cf6530b20df9307f5c79bfebb
zhoubin <zhoubin@qianlima.com>
2021-04-01 12:54:29 +0800

    最小k个数

--- src/SmallestKLCCI.java
+++ src/SmallestKLCCI.java
@@ -17,24 +17,25 @@ public class SmallestKLCCI {
         for (int i = 0; i < 100000000; i++) {
             arr[i] = i;
         }
+//        int[] arr = {1, 3, 5, 7, 2, 4, 6, 8};
         long st = System.currentTimeMillis();
 //        Arrays.sort(arr);
 //        Arrays.stream(arr).limit(10).forEach(System.out::println);
-        ks(arr, 10);
-        System.out.println(System.currentTimeMillis() - st+"ms");
+        int[] ks = smallestK(arr, 10);
+        Arrays.stream(ks).forEach(System.out::println);
+        System.out.println(System.currentTimeMillis() - st + "ms");
     }
 
-    public static void ks(int[] arr, int k){
+    public static int[] ks(int[] arr, int k) {
         //定义输出的结果数组
-        int[]res = new int[k];
+        int[] res = new int[k];
+
+        if (arr.length == 0 || k == 0) {
+            return res;
+        }
 
         //定义优先队列，排序方式为升序
-        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 -o2;
-            }
-        });
+        PriorityQueue<Integer> queue = new PriorityQueue<>((num1, num2) -> num2 - num1);
 
         //先拿出k个数放入队列中
         for (int i = 0; i < k; ++i) {
@@ -44,7 +45,7 @@ public class SmallestKLCCI {
 
         //循环比较数组中每一个元素与队顶的元素大小，peek取出队顶元素，队顶大，删之。并放入数组中的元素
         for (int i = k; i < arr.length; ++i) {
-            if (queue.peek() > arr[i]){
+            if (queue.peek() > arr[i]) {
                 queue.poll();
                 queue.offer(arr[i]);
             }
@@ -54,7 +55,37 @@ public class SmallestKLCCI {
         for (int i = 0; i < k; ++i) {
             res[i] = queue.poll();
         }
+        return res;
+    }
 
-        Arrays.stream(res).forEach(System.out::println);
+    public static int[] smallestK(int[] arr, int k) {
+        if (arr.length == 0 || k == 0) return new int[0];
+        return quickSearch(arr, 0, arr.length - 1, k - 1);
+    }
+
+    public static int[] quickSearch(int[] arr, int low, int high, int target) {
+        int j = partitionArray(arr, low, high);
+        if (j == target) {
+            return Arrays.copyOf(arr, j + 1);
+        }
+        return j < target ? quickSearch(arr, j + 1, high, target) : quickSearch(arr, low, j - 1, target);
+    }
+
+    public static int partitionArray(int[] arr, int i, int j) {
+        int target = arr[i];
+        while (i < j) {
+            while (j > i && arr[j] > target) j--;
+            if (j > i) {
+                arr[i] = arr[j];
+                i++;
+            }
+            while (i < j && arr[i] < target) i++;
+            if (i < j) {
+                arr[j] = arr[i];
+                j--;
+            }
+        }
+        arr[i] = target;
+        return i;
     }
 }
